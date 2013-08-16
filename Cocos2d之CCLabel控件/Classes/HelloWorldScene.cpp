c402f8451131e2d8d1dc968ca014ce2454dcfa15
jianqiang-411 <jianqiang-411@163.com>
2013-08-16 13:22:41 +0800

    ff
    ff

--- Cocos2d之CCLabel控件/Classes/HelloWorldScene.cpp
+++ Cocos2d之CCLabel控件/Classes/HelloWorldScene.cpp
@@ -22,56 +22,36 @@ CCScene* HelloWorld::scene()
 // on "init" you need to initialize your instance
 bool HelloWorld::init()
 {
-    //////////////////////////////
-    // 1. super init first
-    if ( !CCLayer::init() )
-    {
-        return false;
-    }
+    bool  pRet = false;
+    do {
+        
+        CC_BREAK_IF(!CCLayer::init());
+//              CCLabelTTF 每次调用 setString （即改变文字）的时候，一个新的OPENGL 纹理将会被创建.。这意味着setString 和创建一个新的标签一样慢。
+        
+//          所以，当你需要频繁的更新它们的时候，尽可能的不用去使用标签对象。  而应该使用CCLabelAtlas或者是CCLabelBMFont。
+        CCLabelTTF* label = CCLabelTTF::create("LabelTTF", "Marker Felt", 21);
+        label->setPosition(ccp(100, 400));
+        this->addChild(label);
+        
+//         CCLabelBMFont  相当于每次改变只改变了图片坐标，而CCLabelTTF要重新渲染.这个类使用之前，需要添加好字体文件，包括一个图片文件 (**.png) 和一个 字体坐标文件 （**.fnt）。
+        CCLabelBMFont* label2 = CCLabelBMFont::create("CCLabelBMFont", "font_red_14.fnt");
+        label2->setPosition(ccp(200, 400));
+        this->addChild(label2);
+        
+//          因为帧率一直在变，使用CCLabelTTF的话效率太低，因为只是数字所以也犯不上使用 CCLabelBMFont 加载那么大的文字图像，所以使用这个比较合适。
+        CCLabelAtlas* label3 = CCLabelAtlas::create("0.0", "fps_images.png", 16, 24, '.');
+        label3->setPosition(ccp(200, 300));
+        this->addChild(label3);
 
-    /////////////////////////////
-    // 2. add a menu item with "X" image, which is clicked to quit the program
-    //    you may modify it.
-
-    // add a "close" icon to exit the progress. it's an autorelease object
-    CCMenuItemImage *pCloseItem = CCMenuItemImage::create(
-                                        "CloseNormal.png",
-                                        "CloseSelected.png",
-                                        this,
-                                        menu_selector(HelloWorld::menuCloseCallback) );
-    pCloseItem->setPosition( ccp(CCDirector::sharedDirector()->getWinSize().width - 20, 20) );
-
-    // create menu, it's an autorelease object
-    CCMenu* pMenu = CCMenu::create(pCloseItem, NULL);
-    pMenu->setPosition( CCPointZero );
-    this->addChild(pMenu, 1);
-
-    /////////////////////////////
-    // 3. add your codes below...
-
-    // add a label shows "Hello World"
-    // create and initialize a label
-    CCLabelTTF* pLabel = CCLabelTTF::create("Hello World", "Thonburi", 34);
-
-    // ask director the window size
-    CCSize size = CCDirector::sharedDirector()->getWinSize();
-
-    // position the label on the center of the screen
-    pLabel->setPosition( ccp(size.width / 2, size.height - 20) );
-
-    // add the label as a child to this layer
-    this->addChild(pLabel, 1);
-
-    // add "HelloWorld" splash screen"
-    CCSprite* pSprite = CCSprite::create("HelloWorld.png");
-
-    // position the sprite on the center of the screen
-    pSprite->setPosition( ccp(size.width/2, size.height/2) );
-
-    // add the sprite as a child to this layer
-    this->addChild(pSprite, 0);
+        
+        pRet =true;
+    } while (0);
     
-    return true;
+    
+    
+    
+    
+    return pRet;
 }
 
 void HelloWorld::menuCloseCallback(CCObject* pSender)
